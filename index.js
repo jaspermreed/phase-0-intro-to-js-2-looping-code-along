2a40647dd3de106ca93d0efd6d2266dcbd23353b
Jasper Reed <jasper.reed@flatironschool.com>
2021-04-06 16:18:42 -0700

    commit tree 63a10d835d73ad2bd5548a233856a2f40ddd2d76

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+let writeCards = (arr, eventName) => {
+
+  return arr.map(name => `Thank you, ${name}, for the wonderful ${eventName} gift!`)
+}
+
+// function test() {
+//   return 10 
+// }
+
+// let arr = [1,2,3,4,5]
+
+// arr.map(num => num + 1)
+
+// [2,3,4,5,6]
